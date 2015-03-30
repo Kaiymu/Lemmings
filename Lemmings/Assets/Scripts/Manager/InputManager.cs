a1b19454d676ec483fc5c254c38c808e003718a6
Samya <s.khemri@student.isartdigital.com>
2015-03-30 17:53:54 +0200

    adding single behaviour

---
+++ Lemmings/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : SingleBehaviour<InputManager> {
+
+	public bool LeftMouseButton() {
+		if(Input.GetMouseButton(0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public bool RightMouseButton() {
+		if(Input.GetMouseButton(1)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public GameObject GetGameObjectClicked()
+	{
+		Vector3 _pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		RaycastHit2D _hit = Physics2D.Raycast(_pos, Vector2.zero);
+		
+		if(_hit.collider != null)
+		{
+			//Debug.Log (_hit.collider.gameObject.transform.position);
+			return _hit.collider.gameObject;
+		} else {
+			return null;
+		}
+	}
+
+}
---
+++ Lemmings/Assets/Scripts/Manager/LemmingsManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LemmingsManager : MonoBehaviour {
+
+	private bool _leftMouseButton;
+
+	private InputManager _inputManager;
+
+	private GameObject _lemming;
+
+	private void Start() {
+		_inputManager = InputManager.instance;
+	}
+	
+	private void Update() {
+		GetMousePressed();
+
+		if(_leftMouseButton == true) {
+
+			_lemming = _inputManager.GetGameObjectClicked();
+
+			if(_lemming != null) {
+				SaveClickedLemming();
+			}
+		}
+	}
+	
+	private void GetMousePressed() {
+		_leftMouseButton = _inputManager.LeftMouseButton();
+	}
+
+	private void SaveClickedLemming() {
+
+	}
+}
