8177389b46834077be31432eb6b192255451843d
Samya <s.khemri@student.isartdigital.com>
2015-03-31 11:29:59 +0200

    numberOfLemmings updating

--- Lemmings/Assets/Scripts/Manager/GUIManager.cs
+++ Lemmings/Assets/Scripts/Manager/GUIManager.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GUIManager : SingleBehaviour<GUIManager> {
 
+	public Text numberOfLemmings;
+
+	public void SetNumberOfLemmings(int number) {
+		numberOfLemmings.text = number.ToString();
+	}
 }
--- Lemmings/Assets/Scripts/Manager/GameManager.cs
+++ Lemmings/Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,37 @@ using System.Collections.Generic;
 
 public class GameManager : SingleBehaviour<GameManager> {
 
+	[HideInInspector]
 	public List<GameObject> allLemmings;
+	public int numberOfLemmings;
+
+	private GUIManager _GUIManager;
 
 	private void Awake() {
-		allLemmings = new List<GameObject>();
+		SetManager();
+		GetAllLemmings();
+	}
 
+	private void SetManager() {
+		_GUIManager = GUIManager.instance;
+	}
+
+	private void GetAllLemmings() {
+		allLemmings = new List<GameObject>();
+		
 		GameObject[] gos = GameObject.FindGameObjectsWithTag("Lemming");
 		
 		foreach(GameObject lemming in gos) {
 			allLemmings.Add(lemming);
 		}
+		
+		numberOfLemmings = allLemmings.Count;
+
+		_GUIManager.SetNumberOfLemmings(numberOfLemmings);
+	}
+
+	public void SetNumberOfLemmings(int number) {
+		numberOfLemmings += number;
+		_GUIManager.SetNumberOfLemmings(numberOfLemmings);
 	}
 }
