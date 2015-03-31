3860fd4e96fa3e4790ea6cabe5dd69feaff5afa4
Samya <s.khemri@student.isartdigital.com>
2015-03-31 10:24:28 +0200

    adding list of lemmings

--- Lemmings/Assets/Scripts/Manager/GUIManager.cs
+++ Lemmings/Assets/Scripts/Manager/GUIManager.cs
@@ -3,18 +3,4 @@ using System.Collections;
 
 public class GUIManager : SingleBehaviour<GUIManager> {
 
-	public Transform numberOfLemmingsText;
-
-	private GUIText _numberOfLemmingsText;
-	private int numberMaxOfLemmings;
-
-	private void Start() {
-		numberMaxOfLemmings = 3;
-		_numberOfLemmingsText = numberOfLemmingsText.GetComponent<GUIText>();
-	}
-
-	public void NumberOfLemmings(int number) {
-		_numberOfLemmingsText.text = "Number of Lemmings : " + number.ToString();
-	}
-
 }
---
+++ Lemmings/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : SingleBehaviour<GameManager> {
+
+	public List<GameObject> allLemmings;
+
+	private void Awake() {
+		allLemmings = new List<GameObject>();
+
+		GameObject[] gos = GameObject.FindGameObjectsWithTag("Lemming");
+		
+		foreach(GameObject lemming in gos) {
+			allLemmings.Add(lemming);
+		}
+	}
+}
