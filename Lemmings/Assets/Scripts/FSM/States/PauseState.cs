febb6cf83b72ccf994818d09b75b07156aa97122
Fontaine <tom_1993@hotmail.fr>
2015-04-01 23:19:22 +0200

    - Pushing pause state final

--- Lemmings/Assets/Scripts/FSM/States/PauseState.cs
+++ Lemmings/Assets/Scripts/FSM/States/PauseState.cs
@@ -5,6 +5,7 @@ public class PauseState : FSMState<Lemmings> {
 
     private static readonly PauseState instance = new PauseState();
 
+    private float speedAnimation;
     public static PauseState Instance
     {
         get { return instance; }
@@ -13,7 +14,8 @@ public class PauseState : FSMState<Lemmings> {
     public override void Begin(Lemmings o, FSM<Lemmings> fsm)
     {
         //o.animatorLemmings.SetInteger("Lemmings", 0);
-        o.animatorLemmings.enabled = false;
+        speedAnimation = o.animatorLemmings.speed;
+        o.animatorLemmings.speed = 0f;
     }
 
 	public override void Execute(Lemmings o, FSM<Lemmings> fsm) {
@@ -22,6 +24,6 @@ public class PauseState : FSMState<Lemmings> {
 
 	public override void Transition(Lemmings o, FSM<Lemmings> fsm)
     {
-        o.animatorLemmings.enabled = true;
+        o.animatorLemmings.speed = speedAnimation;
     }
 }
