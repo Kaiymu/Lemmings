6c7ac62762d5f61c16c0bbd994b8166120cd2b9d
Samya <s.khemri@student.isartdigital.com>
2015-04-02 00:41:05 +0200

    pushing end level : numberoflemmingssaved and no lemmings anymore - yay

---
+++ Lemmings/Assets/Scripts/Triggers/EndLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+    private int numberOfLemmingsSaved;
+
+    private GameManager _gameManager;
+    private LemmingsManager _lemmingsManager;
+
+    private void Awake() {
+        SetManager();
+        numberOfLemmingsSaved = 0;
+    }
+
+    private void SetManager() {
+        _gameManager = GameManager.instance;
+        _lemmingsManager = LemmingsManager.instance;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Lemming") {
+            numberOfLemmingsSaved++;
+
+            _lemmingsManager.RemoveLemming(collider.gameObject);
+
+            if(_gameManager.allLemmings.Count == 0) {
+                Debug.Log("Il n'y a plus de lemmings");
+            }
+        }
+           
+    }
+}
