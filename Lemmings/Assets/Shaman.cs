e8793973e87ec1e6c470b0389cb6df3865a724b6
Fontaine <tom_1993@hotmail.fr>
2015-04-02 17:07:06 +0200

    - LE VRAI PUSH DES BRAVES

---
+++ Lemmings/Assets/Shaman.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Xml;
+
+public class Shaman : MonoBehaviour {
+
+    public Text shamanText;
+    public TextAsset xmlShamanTextField;
+    private XmlNodeList shamanTextsXML;
+    
+    private int i = 0;
+
+	void Start () {
+        SetXML();
+        XmlNode shamanTextXML = shamanTextsXML[0];
+        shamanText.text = shamanTextXML.Attributes["text"].Value.ToString();
+	}
+
+    private void SetXML() {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xmlShamanTextField.text);
+        shamanTextsXML = xmlDoc.GetElementsByTagName("Shaman");
+    }
+
+    private void Update() {
+        DisplayNextText();
+    }
+
+    private void DisplayNextText() {
+        if(InputManager.instance.LeftMouseButtonDown() && i < shamanTextsXML.Count)
+        {
+            XmlNode shamanTextXML = shamanTextsXML[i];
+            shamanText.text = shamanTextXML.Attributes["text"].Value.ToString();
+            i++;
+        }
+    }
+	
+}
